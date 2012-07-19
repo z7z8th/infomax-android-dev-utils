16dce20c9e3d33a3ba26f63198233c3a87e79485
Bob.Zhao <bob.zhao@infomax.com.tw>
2012-07-19 17:36:46 +0800

    compare time with file on board when push

--- common/push-new-file-of-build.sh
+++ common/push-new-file-of-build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+[ x$1 = x-a ] && PUSH_ALL=1
+
 DEVICE_TYPE=iM9828_evb_v3
 PRODUCT_OUT_DIR=out/target/product/$DEVICE_TYPE
 which adb 2>&1 >/dev/null || export PATH=out/host/linux-x86/bin:$PATH
@@ -9,9 +11,15 @@ adb remount
 
 find $PRODUCT_OUT_DIR/system -newer $PRODUCT_OUT_DIR/system.img | \
     tee new-files.lst | \
-    while read line; do 
-        [ -d "$line" ] && continue;
-        board_path=${line#${PRODUCT_OUT_DIR}};
-        echo -n "-> $board_path : ";
-        adb push $line $board_path;
+    while read new_file_path; do
+        [ -d "$new_file_path" ] && continue;
+        board_file_path=${new_file_path#${PRODUCT_OUT_DIR}};
+        board_file_time=`adb shell stat -c '%Y' $board_file_path | sed -e 's/\r//g'`
+        new_file_time=`stat -c '%Y' $new_file_path`
+        [ "$new_file_time" -le "$board_file_time" -a x$PUSH_ALL != x1 ] && {
+            # echo "not new. skip $board_file_path";
+            continue;
+        }
+        echo -n "push -> $board_file_path : ";
+        adb push $new_file_path $board_file_path;
     done
